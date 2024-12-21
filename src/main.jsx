8024a29bc14422be5e387de7ebaa9b99158a6748
Antonio <contriantoni@gmail.com>
2024-12-21 15:14:01 +0100

    Nuovo progetto nuova vita

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import { MyDataProvider } from '@components/ScrollData';
 import myIcon from '@images/img_topbar/icon.webp';
-import '@styles/App.css';
-import '@fortawesome/fontawesome-free/css/all.css';
 import './i18n';
 
 const App = React.lazy(() => import('./App'));
