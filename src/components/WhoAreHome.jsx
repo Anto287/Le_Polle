46c1263ea8c0b424b13acd383fb1d767dd7844a2
Antonio <contriantoni@gmail.com>
2024-12-30 22:57:54 +0100

    End who are home

--- src/components/WhoAreHome.jsx
+++ src/components/WhoAreHome.jsx
@@ -1,77 +1,53 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, lazy } from "react";
 import { useTranslation } from "react-i18next";
 import { useWindowSizeHook } from "@hooks/useWindowSizeHook";
 import { useScrollData } from "@components/ScrollData";
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Autoplay, EffectFade } from "swiper/modules";
+import { Autoplay, EffectFade, EffectCreative } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/effect-fade";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
+import 'swiper/css/effect-creative';
+
 import ImgLoader from "@components/ImgLoader";
 import "@styles/WhoAreHome.css";
 
 import img_1_mobile from "@images/img_home_desc/mobile/img_1.webp";
 import img_1_tablet from "@images/img_home_desc/tablet/img_1.webp";
-import img_1_desktop from "@images/img_home_desc/desktop/img_1.webp";
-import img_1_large from "@images/img_home_desc/large/img_1.webp";
 
 import img_2_mobile from "@images/img_home_desc/mobile/img_2.webp";
 import img_2_tablet from "@images/img_home_desc/tablet/img_2.webp";
-import img_2_desktop from "@images/img_home_desc/desktop/img_2.webp";
-import img_2_large from "@images/img_home_desc/large/img_2.webp";
 
 import img_3_mobile from "@images/img_home_desc/mobile/img_3.webp";
 import img_3_tablet from "@images/img_home_desc/tablet/img_3.webp";
-import img_3_desktop from "@images/img_home_desc/desktop/img_3.webp";
-import img_3_large from "@images/img_home_desc/large/img_3.webp";
 
 import img_4_mobile from "@images/img_home_desc/mobile/img_4.webp";
 import img_4_tablet from "@images/img_home_desc/tablet/img_4.webp";
-import img_4_desktop from "@images/img_home_desc/desktop/img_4.webp";
-import img_4_large from "@images/img_home_desc/large/img_4.webp";
 
 import img_5_mobile from "@images/img_home_desc/mobile/img_5.webp";
 import img_5_tablet from "@images/img_home_desc/tablet/img_5.webp";
-import img_5_desktop from "@images/img_home_desc/desktop/img_5.webp";
-import img_5_large from "@images/img_home_desc/large/img_5.webp";
 
 import img_6_mobile from "@images/img_home_desc/mobile/img_6.webp";
 import img_6_tablet from "@images/img_home_desc/tablet/img_6.webp";
-import img_6_desktop from "@images/img_home_desc/desktop/img_6.webp";
-import img_6_large from "@images/img_home_desc/large/img_6.webp";
 
 import img_7_mobile from "@images/img_home_desc/mobile/img_7.webp";
 import img_7_tablet from "@images/img_home_desc/tablet/img_7.webp";
-import img_7_desktop from "@images/img_home_desc/desktop/img_7.webp";
-import img_7_large from "@images/img_home_desc/large/img_7.webp";
 
 import img_8_mobile from "@images/img_home_desc/mobile/img_8.webp";
 import img_8_tablet from "@images/img_home_desc/tablet/img_8.webp";
-import img_8_desktop from "@images/img_home_desc/desktop/img_8.webp";
-import img_8_large from "@images/img_home_desc/large/img_8.webp";
 
 import img_9_mobile from "@images/img_home_desc/mobile/img_9.webp";
 import img_9_tablet from "@images/img_home_desc/tablet/img_9.webp";
-import img_9_desktop from "@images/img_home_desc/desktop/img_9.webp";
-import img_9_large from "@images/img_home_desc/large/img_9.webp";
 
 import img_10_mobile from "@images/img_home_desc/mobile/img_10.webp";
 import img_10_tablet from "@images/img_home_desc/tablet/img_10.webp";
-import img_10_desktop from "@images/img_home_desc/desktop/img_10.webp";
-import img_10_large from "@images/img_home_desc/large/img_10.webp";
 
 import img_11_mobile from "@images/img_home_desc/mobile/img_11.webp";
 import img_11_tablet from "@images/img_home_desc/tablet/img_11.webp";
-import img_11_desktop from "@images/img_home_desc/desktop/img_11.webp";
-import img_11_large from "@images/img_home_desc/large/img_11.webp";
 
 import img_12_mobile from "@images/img_home_desc/mobile/img_12.webp";
 import img_12_tablet from "@images/img_home_desc/tablet/img_12.webp";
-import img_12_desktop from "@images/img_home_desc/desktop/img_12.webp";
-import img_12_large from "@images/img_home_desc/large/img_12.webp";
 
 gsap.registerPlugin(useGSAP);
 
@@ -83,74 +59,50 @@ const WhoAreHome = () => {
     {
       mobile: img_1_mobile,
       tablet: img_1_tablet,
-      desktop: img_1_desktop,
-      large: img_1_large,
     },
     {
       mobile: img_2_mobile,
       tablet: img_2_tablet,
-      desktop: img_2_desktop,
-      large: img_2_large,
     },
     {
       mobile: img_3_mobile,
       tablet: img_3_tablet,
-      desktop: img_3_desktop,
-      large: img_3_large,
     },
     {
       mobile: img_4_mobile,
       tablet: img_4_tablet,
-      desktop: img_4_desktop,
-      large: img_4_large,
     },
     {
       mobile: img_5_mobile,
       tablet: img_5_tablet,
-      desktop: img_5_desktop,
-      large: img_5_large,
     },
     {
       mobile: img_6_mobile,
       tablet: img_6_tablet,
-      desktop: img_6_desktop,
-      large: img_6_large,
     },
     {
       mobile: img_7_mobile,
       tablet: img_7_tablet,
-      desktop: img_7_desktop,
-      large: img_7_large,
     },
     {
       mobile: img_8_mobile,
       tablet: img_8_tablet,
-      desktop: img_8_desktop,
-      large: img_8_large,
     },
     {
       mobile: img_9_mobile,
       tablet: img_9_tablet,
-      desktop: img_9_desktop,
-      large: img_9_large,
     },
     {
       mobile: img_10_mobile,
       tablet: img_10_tablet,
-      desktop: img_10_desktop,
-      large: img_10_large,
     },
     {
       mobile: img_11_mobile,
       tablet: img_11_tablet,
-      desktop: img_11_desktop,
-      large: img_11_large,
     },
     {
       mobile: img_12_mobile,
       tablet: img_12_tablet,
-      desktop: img_12_desktop,
-      large: img_12_large,
     },
   ];
 
@@ -206,7 +158,7 @@ const WhoAreHome = () => {
 
   return (
     <div className="container-who-are">
-      {breakpoint === 0 && (
+      {breakpoint === 0 && 
         <div className="container-slider-home-mobile">
           <div className="title-who-are-mobile" ref={titleRef}>
             <h1>{t("WHO_ARE")}</h1>
@@ -254,9 +206,9 @@ const WhoAreHome = () => {
             </Swiper>
           </div>
         </div>
-      )}
+      }
 
-      {breakpoint === 1 && (
+      {breakpoint === 1 && 
         <div className="container-slider-home-tablet">
           <div className="title-who-are-tablet" ref={titleRef}>
             <h1>{t("WHO_ARE")}</h1>
@@ -292,10 +244,12 @@ const WhoAreHome = () => {
                       styleImgLoader={{
                         width: "100%",
                         height: "50vh",
+                        minHeight: "400px"
                       }}
                       style={{
                         width: "100%",
                         height: "50vh",
+                        minHeight: "300px"
                       }}
                       styleImg={{
                         objectFit: "cover",
@@ -307,7 +261,131 @@ const WhoAreHome = () => {
             </div>
           </div>
         </div>
-      )}
+      }
+
+      {breakpoint === 2 && 
+        <div className="container-slider-home-pc">
+          <div className="title-who-are-pc" ref={titleRef}>
+            <h1>{t("WHO_ARE")}</h1>
+          </div>
+
+          <div className="container-swiper-home-pc">
+            <div className="container-who-are-pc" ref={swiperTextRef}>
+              <div className="container-text-who-are-pc">
+                <b>{t("WHO_ARE_TITLE")}</b>
+                {t("WHO_ARE_LONG_DESC")}
+              </div>
+            </div>
+
+            <div className="container-swiper-pc" ref={swiperRef}>
+              <Swiper
+                grabCursor={true}
+                effect={'creative'}
+                creativeEffect={{
+                  prev: {
+                    shadow: true,
+                    translate: [0, 0, -400],
+                  },
+                  next: {
+                    translate: ['100%', 0, 0],
+                  },
+                }}
+                modules={[Autoplay, EffectCreative]}
+                loop={true}
+                allowTouchMove={true}
+                speed={2000}
+                autoplay={{
+                  delay: 5000,
+                  disableOnInteraction: false,
+                }}
+              >
+                {listImg.filter((el, index) => index % 2 == 0).map((el, index) => (
+                  <SwiperSlide key={index}>
+                    <ImgLoader
+                      src={el.mobile}
+                      styleImgLoader={{
+                        width: "25vw",
+                        height: "60vh",
+                        minHeight: "400px",
+                      }}
+                      style={{
+                        width: "25vw",
+                        height: "60vh",
+                        minHeight: "400px"
+                      }}
+                      styleImg={{
+                        objectFit: "cover",
+                      }}
+                    />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
+          </div>
+        </div>
+      }
+
+      {breakpoint === 3 && 
+        <div className="container-slider-home-large">
+          <div className="title-who-are-large" ref={titleRef}>
+            <h1>{t("WHO_ARE")}</h1>
+          </div>
+
+          <div className="container-swiper-home-large">
+            <div className="container-who-are-large" ref={swiperTextRef}>
+              <div className="container-text-who-are-large">
+                <b>{t("WHO_ARE_TITLE")}</b>
+                {t("WHO_ARE_LONG_DESC")}
+              </div>
+            </div>
+
+            <div className="container-swiper-large" ref={swiperRef}>
+              <Swiper
+                grabCursor={true}
+                effect={'creative'}
+                creativeEffect={{
+                  prev: {
+                    shadow: true,
+                    translate: [0, 0, -400],
+                  },
+                  next: {
+                    translate: ['100%', 0, 0],
+                  },
+                }}
+                modules={[Autoplay, EffectCreative]}
+                loop={true}
+                allowTouchMove={true}
+                speed={2000}
+                autoplay={{
+                  delay: 5000,
+                  disableOnInteraction: false,
+                }}
+              >
+                {listImg.filter((el, index) => index % 2 == 0).map((el, index) => (
+                  <SwiperSlide key={index}>
+                    <ImgLoader
+                      src={el.tablet}
+                      styleImgLoader={{
+                        width: "20vw",
+                        height: "50vh",
+                        minHeight: "400px",
+                      }}
+                      style={{
+                        width: "20vw",
+                        height: "50vh",
+                        minHeight: "400px"
+                      }}
+                      styleImg={{
+                        objectFit: "cover",
+                      }}
+                    />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
+          </div>
+        </div>
+      }
     </div>
   );
 };
